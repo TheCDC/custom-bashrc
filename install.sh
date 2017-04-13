e3fa24fcbdb80690c7c7f4f551ef8b72caa75f2c
Christopher Chen <christopher.chen1995@gmail.com>
2017-04-13 01:44:27 -0700

    script works

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
-# rm ~/.bashrc
-ln -s ~/custom-bashrc ~/.bashrc
+#!/bin/bash
+cd ~/
+if [ ! -f "~/.bashrc" ]; then
+	# echo ".bashrc exists"
+	d=`date '+%Y_%m_%d__%H_%M_%S'`;
+	target=".old-bashrc/.bashrc-$d"
+	old=".bashrc"
+	echo "Moving $old to $target"
+	mkdir -p .old-bashrc
+	touch $target
+	mv -v $old $target
+	# rm $old
+fi
+ln -s ~/custom-bashrc/.bashrc ~/.bashrc
